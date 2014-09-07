e8c733c5aac861a24867cd6858792d5f78c4ba1e
Leslie Harrison <lesliehh@live.com>
2014-09-07 14:28:07 -0500

    update

--- plot1.R
+++ plot1.R
@@ -2,42 +2,40 @@ setwd("C:/Base/Education/MOOC/Johns Hopkins - Data Science Specialization/Explor
 
 file <- "../data/household_power_consumption.txt";
 
-start = "2006-12-16 17:24:00";
-from  = "2007-02-01 00:00:00";
-to    = "2007-02-01 23:59:00";
-
-colNames = c("Date", "Time", 
-             "Global_active_power", 
-             "Global_reactive_power", 
-             "Voltage",
-             "Global_intensity", 
-             "Sub_metering_1", 
-             "Sub_metering_2", 
-             "Sub_metering_3");
-
-daysDiff = as.numeric(difftime(from, start));
-minutesInADay = 24 * 60;
-maxRead = 2 * minutesInADay;
-numSkip = daysDiff * minutesInADay;
-
 df = read.table(file, 
                 sep = ";",
-                col.names = colNames,
-                na.strings = "?", 
-                nrows = maxRead, 
-                skip = numSkip);
+                na.strings = "?",
+                header = TRUE);
+
+# Could add stringsAsFactors=FALSE to the settings above 
+# so that it does not import data as factors, which are a pain
+# but is not necessary as paste() returns as character
+
+# Convert the date factors to dates and make it the index of df
+comb <- paste(df$Date, df$Time)
+dates <- strptime(comb, format="%d/%m/%Y %H:%M:%S")
+#dates <- as.Date(dates, format="%d/%m/%Y %H:%M:%S")
+rownames(df) <- dates
+
+# Declare the start-to values
+from  = "2007-02-01 00:00:00";
+to    = "2007-02-01 23:59:00";
+# Obtain the row values of the start-to values
+fromInt <- which(rownames(df) == from)
+toInt   <- which(rownames(df) == to)
+# Subset the data with the start-to values and exclude the Date/Time columns
+df <- df[fromInt:toInt,3:9]
+
+
 
 
 #### PLOT 1
+
 # Open PNG file
-png(file = "plot1.png");
+png(file = "plot1.png", width = 480, height = 480);
 
 # Create the plot
-hist(df$Global_active_power, 
-     freq = 200, 
-     col = "red", 
-     ann=FALSE);
-
+hist(df$Global_active_power, freq = 200, col = "red", ann=FALSE);
 title(main = "Global Active Power", 
       ylab="Frequency",
       xlab="Global Active Power (kilowatts)");
--- plot3.R
+++ plot3.R
@@ -2,42 +2,43 @@ setwd("C:/Base/Education/MOOC/Johns Hopkins - Data Science Specialization/Explor
 
 file <- "../data/household_power_consumption.txt";
 
-start = "2006-12-16 17:24:00";
-from  = "2007-02-01 00:00:00";
-to    = "2007-02-01 23:59:00";
-
-colNames = c("Date", "Time", 
-             "Global_active_power", 
-             "Global_reactive_power", 
-             "Voltage",
-             "Global_intensity", 
-             "Sub_metering_1", 
-             "Sub_metering_2", 
-             "Sub_metering_3");
-
-daysDiff = as.numeric(difftime(from, start));
-minutesInADay = 24 * 60;
-maxRead = 2 * minutesInADay;
-numSkip = daysDiff * minutesInADay;
-
 df = read.table(file, 
                 sep = ";",
-                col.names = colNames,
-                na.strings = "?", 
-                nrows = maxRead, 
-                skip = numSkip);
+                na.strings = "?",
+                header = TRUE);
+
+# Could add stringsAsFactors=FALSE to the settings above 
+# so that it does not import data as factors, which are a pain
+# but is not necessary as paste() returns as character
+
+# Convert the date factors to dates and make it the index of df
+comb <- paste(df$Date, df$Time)
+dates <- strptime(comb, format="%d/%m/%Y %H:%M:%S")
+#dates <- as.Date(dates, format="%d/%m/%Y %H:%M:%S")
+rownames(df) <- dates
+
+# Declare the start-to values
+from  = "2007-02-01 00:00:00";
+to    = "2007-02-02 23:59:00";
+# Obtain the row values of the start-to values
+fromInt <- which(rownames(df) == from)
+toInt   <- which(rownames(df) == to)
+# Subset the data with the start-to values and exclude the Date/Time columns
+df <- df[fromInt:toInt,3:9]
+
+
 
 
 #### PLOT 3
+
 # Open PNG file
-png(file = "plot3.png");
+png(file = "plot3.png", width = 480, height = 480);
 
 # Create the plot
-plot(df$Sub_metering_1, type="l", axes=FALSE, ann=FALSE);
-lines(df$Sub_metering_2, type="l", col="red");
-lines(df$Sub_metering_3, type="l", col="blue");
-axis(1, at=c(1,1450,2900),lab=c("Thu","Fri","Sat"));
-axis(2, las=1, at=10*0:3);
+dates <- strptime(rownames(df), format="%Y-%m-%d %H:%M:%S")
+plot(x = dates, y = df$Sub_metering_1, type="l", ann=FALSE);
+lines(x = dates, y = df$Sub_metering_2, type="l", col="red");
+lines(x = dates, y = df$Sub_metering_3, type="l", col="blue");
 title(ylab="Energy sub metering");
 legend("topright", 
        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
